be54518af5650e8f1f65d8f75d08d6f53c322ef4
younjun.hwang <youngjun.hwang@cubox.ai>
2023-08-06 18:27:44 +0900

    세개의 종목에 대한 크롤링

--- crawling_stock_ver2.py
+++ crawling_stock_ver2.py
@@ -6,23 +6,39 @@ import os
 options = webdriver.ChromeOptions()
 browser = webdriver.Chrome()
 
-#1. 페이지 이동
-url = "https://navercomp.wisereport.co.kr/v2/company/c1010001.aspx?cmp_cd=005930"
-browser.get(url)
+code = ['098120', '009520', '005930']
 
-# 재무재표 연간 탭 클릭
-checkbox = browser.find_element(By.ID, 'cns_Tab21')
-checkbox.click()
+for i in code:
+    #1. 페이지 이동
+    url = "https://navercomp.wisereport.co.kr/v2/company/c1010001.aspx?cmp_cd=" + i
+    browser.get(url)
 
-array1 = []
+    # 재무재표 연간 탭 클릭
+    checkbox = browser.find_element(By.ID, 'cns_Tab21')
+    checkbox.click()
 
-row1 = browser.find_elements(By.CLASS_NAME, 'center')
+    data_array = []
 
-for row in row1:
-    if row.text == "2020(A)":
-        print("있음")
-        parent = row.find_element(By.XPATH, "..")
-        eles = parent.find_element(By.XPATH, '//*[@id="cTB25"]/tbody/tr[1]/td[6]')
-        print(eles.text)
+    row1 = browser.find_elements(By.CLASS_NAME, 'center')
 
-
+    for row in row1:
+        if row.text == "2022(A)":
+            parent = row.find_element(By.XPATH, "..")
+            eles = parent.find_element(By.XPATH, '//*[@id="cTB25"]/tbody/tr[3]/td[6]')
+            data_array.append(eles.text)
+        elif row.text == "2023(E)":
+            parent = row.find_element(By.XPATH, "..")
+            eles = parent.find_element(By.XPATH, '//*[@id="cTB25"]/tbody/tr[4]/td[6]')
+            if eles.text == '':
+                data_array.append(0)
+            else:
+                data_array.append(eles.text)
+        elif row.text == "2024(E)":
+            parent = row.find_element(By.XPATH, "..")
+            eles = parent.find_element(By.XPATH, '//*[@id="cTB25"]/tbody/tr[5]/td[6]')  
+            if eles.text == '':
+                data_array.append(0)
+            else:
+                data_array.append(eles.text)
+    
+    print(data_array)
