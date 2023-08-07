ba0c5665426db6c01f5d541cd4d0ad1784ae535d
yjhwang-cubox-ai <youngjun.hwang@cubox.ai>
2023-08-08 08:20:48 +0900

    test

--- crawling_stock_ver4.py
+++ crawling_stock_ver4.py
@@ -10,8 +10,8 @@ from time import sleep
 result_consensus = []
 
 def StockCodeList(path):
-    stock_code = pd.read_excel(path, sheet_name = 'Sheet1', converters={'단축코드':str})
-    stock_code = stock_code[['한글 종목명','단축코드']]
+    stock_code = pd.read_excel(path, sheet_name = 'Sheet1', converters={'종목코드':str})
+    stock_code = stock_code[['종목명','종목코드']]
     stock_code.columns = ['Stock','Code']
     # print(stock_code)
 
